b2f46a9b53afaf561e4d7aa77741ec3f5190f0fe
Thomas Ellwood <taellwood@gmail.com>
2024-02-18 20:44:24 +0000

    coach responds with 'stupic question' if questions ends with '?'

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,13 @@
 class QuestionsController < ApplicationController
   def ask
-
   end
 
   def answer
     @answer = params[:answer]
 
-    if @answer == "I am going to work"
+    if @answer.end_with?("?")
+      @coach_answer = "Silly question, get dressed and go to work!"
+    elsif  @answer == "I am going to work"
       @coach_answer = "Great!"
     end
   end
