db713a6cc2839026458f16824d6f77434da81462
Thomas Ellwood <taellwood@gmail.com>
2024-02-18 20:36:00 +0000

    created form feeding answer through to View for Answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,13 @@
 class QuestionsController < ApplicationController
   def ask
+
   end
 
   def answer
+    @answer = params[:answer]
+
+    # if params[:answer]
+    #   @members = @members.select { |member| member.start_with?(params[:member]) }
+    # end
   end
 end
